6c9ac4647cbf6e00364333447e4c08bfcb4dd168
samyak <samyaksk10@gmail.com>
2025-06-24 11:00:57 +0530

    configured the .env file

--- Server-side/src/main/java/com/tracker/expense_tracker/ExpenseTrackerApplication.java
+++ Server-side/src/main/java/com/tracker/expense_tracker/ExpenseTrackerApplication.java
@@ -3,11 +3,16 @@ package com.tracker.expense_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
+		// Load .env variables
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
+
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
-
 }
